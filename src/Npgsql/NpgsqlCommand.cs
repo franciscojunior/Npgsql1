b769cabb2b1ede308095f0349ee2793d2e6a0b8e
fxjr <>
2002-09-08 00:33:19 +0000

    - Updated Copyright statement. - Implemented some methods to get NpgsqlDataAdapter working. - Updated to use the State and Mediator Pattern code. - Created GetCommandText() method to parse command string and provide initial   parameter support.

--- src/Npgsql/NpgsqlCommand.cs
+++ src/Npgsql/NpgsqlCommand.cs
@@ -5,7 +5,7 @@
 // Author:
 //	Francisco Jr. (fxjrlists@yahoo.com.br)
 //
-//	Copyright (C) 2002 Francisco Jr.
+//	Copyright (C) 2002 The Npgsql Development Team
 //
 
 // This library is free software; you can redistribute it and/or
@@ -29,17 +29,17 @@ using System.Net;
 using System.Net.Sockets;
 using System.IO;
 using System.Text;
+using System.Collections;
 
 namespace Npgsql
 {
 	public sealed class NpgsqlCommand : IDbCommand
 	{
 		
-		private NpgsqlConnection 			connection;
-		//private TcpClient					tcp_connection;
-		private String						text;
-		private Int32						timeout;
-		private CommandType					type;
+		private NpgsqlConnection 					connection;
+		private String										text;
+		private Int32											timeout;
+		private CommandType								type;
 		private NpgsqlParameterCollection	parameters;
 		
     // Logging related values
@@ -122,7 +122,7 @@ namespace Npgsql
 			set
 			{
 				connection = (NpgsqlConnection) value;
-				NpgsqlEventLog.LogMsg("Set " + CLASSNAME + ".IDbCommand.Connection", LogLevel.Normal);
+				NpgsqlEventLog.LogMsg("Set " + CLASSNAME + ".IDbCommand.Connection", LogLevel.Debug);
 			}
 		}
 		
@@ -130,13 +130,14 @@ namespace Npgsql
 		{
 			get
 			{
+				NpgsqlEventLog.LogMsg(CLASSNAME + ".get_Connection", LogLevel.Debug);
 				return connection;
 			}
 			
 			set
 			{
 				connection = value;
-				NpgsqlEventLog.LogMsg("Set " + CLASSNAME + ".Connection", LogLevel.Normal);
+				NpgsqlEventLog.LogMsg("Set " + CLASSNAME + ".Connection", LogLevel.Debug);
 			}
 		}
 		
@@ -152,6 +153,7 @@ namespace Npgsql
 		{
 			get
 			{
+				NpgsqlEventLog.LogMsg(CLASSNAME + ".get_Parameters", LogLevel.Debug);
 				return parameters;
 			}
 		}
@@ -173,7 +175,12 @@ namespace Npgsql
 		{
 			get
 			{
-				throw new NotImplementedException();
+				
+				NpgsqlEventLog.LogMsg("Entering " + CLASSNAME + ".get_UpdatedRowSource()", LogLevel.Debug);
+				// [FIXME] Strange, the line below doesn't appears in the stack trace.
+				
+				//throw new NotImplementedException();
+				return UpdateRowSource.Both;
 			}
 			
 			set
@@ -181,7 +188,6 @@ namespace Npgsql
 				throw new NotImplementedException();
 			}
 		}
-		
 		
 		public void Cancel()
 		{
@@ -213,126 +219,77 @@ namespace Npgsql
 			// Check the connection state.
 			CheckConnectionState();
 			
-			NetworkStream network_stream = connection.tcp_connection.GetStream();
-			BufferedStream output_stream = new BufferedStream(network_stream);
+			connection.Query(this);
 			
-			// Send the query to server.
-			// Write the byte 'Q' to identify a query message.
-			output_stream.WriteByte((byte)'Q');
+			// Check if there were any errors.
+			// [FIXME] Just check the first error.
+			if (connection.Mediator.Errors.Count > 0)
+				throw new NpgsqlException(connection.Mediator.Errors[0].ToString());
 			
-			// Write the query. In this case it is the CommandText text.
-			// It is a string terminated by a C NULL character.
-			output_stream.Write(connection.encoding.GetBytes(text + '\x00') , 0, text.Length + 1);
+			// The only expected result is the CompletedResponse result.
 			
-			// Send bytes.
-			output_stream.Flush();
-			
-			// Now, enter in the loop to process the response.
-			
-			Boolean ready_for_query = false;
-			String error_message = null;			
-			
-			Int32 rows_affected = -1;
-			
-			
-			while (!ready_for_query)
-			{
-				switch (network_stream.ReadByte())
-				{
-					case 'C':
-						// This is the CompletedResponse message.
+			String[] ret_string_tokens = ((String)connection.Mediator.GetCompletedResponses()[0]).Split(null);	// whitespace separator.
 						
-						// Get the string returned.
-						String ret_string = PGUtil.ReadString(network_stream, connection.encoding);
-						String[] ret_string_tokens = ret_string.Split(null);	// whitespace separator.
-						
-						// Check if the command was insert, delete or update.
-						// Only theses commands return rows affected.
-						// [FIXME] Is there a better way to check this??
-						if ((String.Compare(ret_string_tokens[0], "INSERT", true) == 0) ||
-						    (String.Compare(ret_string_tokens[0], "UPDATE", true) == 0) ||
-						    (String.Compare(ret_string_tokens[0], "DELETE", true) == 0))
-						    
-							// The number of rows affected is in the third token for insert queries
-							// and in the second token for update and delete queries.
-							// In other words, it is the last token in the 0-based array.
-													
-							rows_affected = Int32.Parse(ret_string_tokens[ret_string_tokens.Length - 1]);
-					
-					
-						// Now wait for ReadyForQuery message.
-						break;
-					
-					case 'P':
-						// This is the cursor response message. 
-						// It is followed by a C NULL terminated string with the name of 
-						// the cursor in a FETCH case or 'blank' otherwise.
-						// In this case it should be always 'blank'.
-						// [FIXME] Get another name for this function.
-						
-						//String cursor_name = GetStringFromNetStream(network_stream);
-						String cursor_name = PGUtil.ReadString(network_stream, connection.encoding);
-						// Continue wainting for ReadyForQuery message.
-						break;
-
-					case 'I':
-						// This is the EmptyQueryResponse.
-						
-						// [FIXME] Just ignore it this way?
-						PGUtil.ReadString(network_stream, connection.encoding);
-						break;
-					
-					case 'E':
-						// This is the ErrorResponse.
-						
-						// Get the string error returned and throw a NpgsqlException.
-						error_message = PGUtil.ReadString(network_stream, connection.encoding);
-					
-						// Even when there is an error, the backend send the ReadyForQuery message.
-						// So, continue waiting for this message.
-						break;
-					case 'Z':
-						// This is the ReadyForQuery message.
-						// It indicates the process termination.
-						ready_for_query = true;
-						break;
-					
-					default:
-						// This is a message that we should handle and we don't.
-						// Throw a NpgsqlException saying that.
-						// [FIXME] Better exception handling. Close the connection???
-						// This is really ugly!! Right now, the message that wasn`t handled
-						// isn't specified! 
-						
-						throw new NpgsqlException("Bug! A message should be handled in ExecuteNonQuery.");
-				}
-			}
+			// Check if the command was insert, delete or update.
+			// Only theses commands return rows affected.
+			// [FIXME] Is there a better way to check this??
+			if ((String.Compare(ret_string_tokens[0], "INSERT", true) == 0) ||
+			    (String.Compare(ret_string_tokens[0], "UPDATE", true) == 0) ||
+			    (String.Compare(ret_string_tokens[0], "DELETE", true) == 0))
+			    
+				// The number of rows affected is in the third token for insert queries
+				// and in the second token for update and delete queries.
+				// In other words, it is the last token in the 0-based array.
+										
+				return Int32.Parse(ret_string_tokens[ret_string_tokens.Length - 1]);
+			else
+				return -1;
 			
-			if (error_message != null)
-				throw new NpgsqlException(error_message);
-			
-			return rows_affected;
 		}
 		
-		public IDataReader ExecuteReader()
+		IDataReader IDbCommand.ExecuteReader()
+		{
+		  NpgsqlEventLog.LogMsg("Entering " + CLASSNAME + ".ExecuteReader() implicit impl", LogLevel.Debug);
+		  
+			return (NpgsqlDataReader) ExecuteReader();
+		}
+		
+		IDataReader IDbCommand.ExecuteReader(CommandBehavior cb)
+		{
+		  NpgsqlEventLog.LogMsg("Entering " + CLASSNAME + ".ExecuteReader() implicit impl(cb)", LogLevel.Debug);
+		  
+			return (NpgsqlDataReader) ExecuteReader(cb);
+			
+		}
+		
+		public NpgsqlDataReader ExecuteReader()
 		{
 		  NpgsqlEventLog.LogMsg("Entering " + CLASSNAME + ".ExecuteReader()", LogLevel.Debug);
+		  
+			
+			return ExecuteReader(CommandBehavior.Default);
+			
+		}
+		
+		public NpgsqlDataReader ExecuteReader(CommandBehavior cb)
+		{
+			// [FIXME] No command behavior handling.
+			
+			NpgsqlEventLog.LogMsg("Entering " + CLASSNAME + ".ExecuteReader(CommandBehavior)", LogLevel.Debug);
 		  
 			// Check the connection state.
 			CheckConnectionState();
 			
-			throw new NotImplementedException();	
-		}
-		
-		public IDataReader ExecuteReader(CommandBehavior cb)
-		{
-		  NpgsqlEventLog.LogMsg("Entering " + CLASSNAME + ".ExecuteReader()", LogLevel.Debug);
-		  
-			// Check the connection state.
-			CheckConnectionState();
+			connection.Query(this);
 			
-			throw new NotImplementedException();	
+			// Check if there were any errors.
+			// [FIXME] Just check the first error.
+			if (connection.Mediator.Errors.Count > 0)
+				throw new NpgsqlException(connection.Mediator.Errors[0].ToString());
 			
+			
+			// Get the resultsets and create a Datareader with them.
+			return new NpgsqlDataReader(connection.Mediator.GetResultSets(), connection);
 		}
 		
 		public Object ExecuteScalar()
@@ -342,143 +299,56 @@ namespace Npgsql
 			// Check the connection state.
 			CheckConnectionState();
 			
-			// [FIXME] This code was copied from ExecuteNonQuery.
-			// Maybe should it be put in a private util method?? 
+			connection.Query(this);	// If there is any error, an NpgsqlException will be thrown.
 			
-			NetworkStream network_stream = connection.tcp_connection.GetStream();
-			BufferedStream output_stream = new BufferedStream(network_stream);
+			//ArrayList results = connection.Mediator.Data;
 			
-			// Send the query to server.
-			// Write the byte 'Q' to identify a query message.
-			output_stream.WriteByte((byte)'Q');
+			Object result = null;	// Result of the ExecuteScalar().
 			
-			// Write the query. In this case it is the CommandText text.
-			// It is a string terminated by a C NULL character.
-			PGUtil.WriteString(text, output_stream, connection.encoding);
 			
-			// Send bytes.
-			output_stream.Flush();
+			// Now get the results.
+			// Only the first column of the first row must be returned.
 			
-			// Now, enter in the loop to process the response.
+			// Get ResultSets.
+			ArrayList resultSets = connection.Mediator.GetResultSets();
 			
-			Boolean ready_for_query = false;
-			String error_message = null;			
 			
-			Object result = null;
+			// First data is the RowDescription object.
+			//NpgsqlRowDescription rd = (NpgsqlRowDescription)results[0];
 			
-			// Used in the RowDescription, AsciiRow and BinaryRow messages.
-			NpgsqlRowDescription rd = null;
+			NpgsqlResultSet firstResultSet = (NpgsqlResultSet)resultSets[0];
 			
-			Int32 message;
+			NpgsqlRowDescription rd = firstResultSet.RowDescription;
+						
+			NpgsqlAsciiRow ascii_row = (NpgsqlAsciiRow)firstResultSet[0];
 			
-			while (!ready_for_query)
+			// Now convert the string to the field type.
+			
+			// [FIXME] Hardcoded values for int types and string.
+			// Change to NpgsqlDbType.
+			// For while only int4 and string are strong typed.
+			// Any other type will be returned as string.
+			
+			switch (rd[0].type_oid)
 			{
-				message = network_stream.ReadByte();
-				switch (message)
-				{
-					case 'C':
-						// This is the CompletedResponse message.
-						// Get the string returned.
-						
-						String ret_string = PGUtil.ReadString(network_stream, connection.encoding);
-						String[] ret_string_tokens = ret_string.Split(null);	// whitespace separator.
-						
-						// [FIXME] Just ignore the command string returned?
-						
-						// Now wait for ReadyForQuery message.
-						break;
-					
-					case 'P':
-						// This is the cursor response message. 
-						// It is followed by a C NULL terminated string with the name of 
-						// the cursor in a FETCH case or 'blank' otherwise.
-						// In this case it should be always 'blank'.
-						// [FIXME] Get another name for this function.
-						// String cursor_name = GetStringFromNetStream(network_stream);
-						String cursor_name = PGUtil.ReadString(network_stream, connection.encoding);
-						
-						// Continue wainting for ReadyForQuery message.
-						break;
-
-					case 'T':
-						// This is the RowDescription message.
-						
-						rd = new NpgsqlRowDescription();
-						rd.ReadFromStream(network_stream, connection.encoding);
-						
-						// Now wait for the AsciiRow messages.
-						break;
-						
-					case 'D':
-						// This is the AsciiRow message.
-						
-						NpgsqlAsciiRow ascii_row = new NpgsqlAsciiRow(rd);
-						ascii_row.ReadFromStream(network_stream, connection.encoding);
-						
-						// Just get the first row.
-						if(result == null)
-							// Now convert the string to the field type.
-							// [FIXME] Hardcoded values for int types and string.
-							// Change to NpgsqlDbType.
-							// For while only int4 and string are strong typed.
-							// Any other type will be returned as string.
-							
-							switch (rd[0].type_oid)
-							{
-								case 23:	// int4, integer.
-									result = Convert.ToInt32(ascii_row[0]);
-									break;
-								case 25:  // text
-									// Get only the first column.
-									result = ascii_row[0];
-									break;
-								default:
-									NpgsqlEventLog.LogMsg("Unrecognized datatype returned by ExecuteScalar():" + 
-									                      rd[0].type_oid + " Returning String...", LogLevel.Debug);
-									result = ascii_row[0];
-									break;
-							}
-						
-						// Now wait for CompletedResponse message.
-						break;
-					
-					case 'I':
-						// This is the EmptyQueryResponse.
-						
-						// [FIXME] Just ignore it this way?
-						PGUtil.ReadString(network_stream, connection.encoding);
-						break;
-					
-					case 'E':
-						// This is the ErrorResponse.
-						// Get the string error returned and throw a NpgsqlException.
-						
-						error_message = PGUtil.ReadString(network_stream, connection.encoding);
-					
-						// Even when there is an error, the backend send the ReadyForQuery message.
-						// So, continue waiting for this message.
-						break;
-					case 'Z':
-						// This is the ReadyForQuery message.
-						// It indicates the process termination.
-						ready_for_query = true;
-						break;
-					
-					default:
-						// This is a message that we should handle and we don't.
-						// Throw a NpgsqlException saying that.
-						// [FIXME] Better exception handling. Close the connection???
-												
-						throw new NpgsqlException("Bug! A message should be handled in ExecuteNonQuery. Message as Int32 = " + message);
-				}
+				case 23:	// int4, integer.
+					result = Convert.ToInt32(ascii_row[0]);
+					break;
+				case 25:  // text
+					// Get only the first column.
+					result = ascii_row[0];
+					break;
+				default:
+					NpgsqlEventLog.LogMsg("Unrecognized datatype returned by ExecuteScalar():" + 
+					                      rd[0].type_oid + " Returning String...", LogLevel.Debug);
+					result = ascii_row[0];
+					break;
 			}
-			
-			if (error_message != null)
-				throw new NpgsqlException(error_message);
 			
 			return result;
 			
 		}
+		
 		
 		
 		public void Prepare()
@@ -515,6 +385,36 @@ namespace Npgsql
 			if (connection.State != ConnectionState.Open)
 				throw new InvalidOperationException("The Connection is not open");
 			
-		}						
+		}
+		
+		///<summary>
+		/// This method substitutes the parameters, if exist, in the command
+		/// to their actual values.
+		/// The parameter name format is <b>:ParameterName</b>.
+		/// </summary>
+		/// 
+		
+		public String GetCommandText()
+		{
+			NpgsqlEventLog.LogMsg("Entering " + CLASSNAME + ".GetCommandText()", LogLevel.Debug);
+			// If there is no parameters, just return the text.
+			if (parameters.Count == 0)
+				return text;
+			
+			String result = text;
+			String parameterName;
+			
+			// Now check if the parameters are there and replace them for the actual values.
+			for (Int32 i = 0; i < parameters.Count; i++)
+			{
+				parameterName = ((NpgsqlParameter)parameters[i]).ParameterName;
+				if (result.IndexOf(parameterName) <= 0)
+					throw new NpgsqlException("Parameter :" + parameterName + " wasn't found in the query.");
+				
+				result = result.Replace(":" + parameterName, ((NpgsqlParameter)parameters[i]).Value.ToString());
+			}
+						
+			return result;
+		}
 	}
 }
