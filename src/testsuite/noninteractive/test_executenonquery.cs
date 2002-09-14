5a93ac121af6e19f363de6bad6a6592adec86f53
mcmicro <>
2002-09-14 22:02:08 +0000

    Added tests for the update and delete SQL statements. Added an expected_* file for the executenonquery case

--- src/testsuite/noninteractive/test_executenonquery.cs
+++ src/testsuite/noninteractive/test_executenonquery.cs
@@ -16,10 +16,21 @@ public class test_executenonquery
 			conn.Open();
 			Console.WriteLine("Connection completed");
 			
+			// Check whether the insert statement works
 			NpgsqlCommand command = new NpgsqlCommand("insert into tablea(field_text) values ('Text from Npgsql');", conn);
 			Int32 num_rows = command.ExecuteNonQuery();
 			Console.WriteLine("{0} rows were added!", num_rows);
 			
+			// Check whether the update statement works
+			command.CommandText = "update tablea set field_text='Updated Text from Npgsql' where field_text='Text from Npgsql';";
+			num_rows = command.ExecuteNonQuery();
+			Console.WriteLine("{0} rows were updated!", num_rows);
+
+			// Check whether the delete statement works
+			command.CommandText = "delete from tablea where field_text = 'Updated Text from Npgsql'";
+			num_rows = command.ExecuteNonQuery();
+			Console.WriteLine("{0} rows were deleted!", num_rows);
+
 		}
 		catch(NpgsqlException e)
 		{
