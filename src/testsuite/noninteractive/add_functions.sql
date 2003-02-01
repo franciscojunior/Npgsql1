80baed5527ae0eec578e3eb9d356c749d50111b6
fxjr <>
2003-02-01 03:42:37 +0000

    Added some functions and changed the language and body of the first because postgresql 7.3 was complaining about the syntax.

--- src/testsuite/noninteractive/add_functions.sql
+++ src/testsuite/noninteractive/add_functions.sql
@@ -10,8 +10,23 @@
 -- Used in test_1
 --
 create function funcA() returns int as '
-declare
-begin
-     return 0;
-end;
-' language 'plpgsql';
+     select 0;
+
+' language 'sql';
+
+
+create function funcB() returns setof tablea as ' 
+select * from tablea;
+' language 'sql';
+
+
+create function funcC() returns int8 as '
+select count(*) from tablea;
+' language 'sql';
+
+
+create function funcC(int4) returns int8 as '
+select count(*) from tablea where field_int4 = $1;
+' language 'sql';
+
+
