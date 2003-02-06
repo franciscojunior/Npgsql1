8ededb1e938ac215f6044045a1f3d71a6b76c6ad
fxjr <>
2003-02-06 16:58:38 +0000

    Updated test and expected result to work regardless of the server version used. 7.2 returns int4 from select count(*) and 7.3 returns int8. So only get an object and write its content.

--- src/testsuite/noninteractive/test_executescalar.cs
+++ src/testsuite/noninteractive/test_executescalar.cs
@@ -35,10 +35,9 @@ public class test_executescalar
 			NpgsqlCommand command = new NpgsqlCommand();
 			command.CommandText = "select count(*) from tablea";
 			command.Connection = conn;
-			Int32 result = (Int32) command.ExecuteScalar();
+			Object result = command.ExecuteScalar();
 			Console.WriteLine(result.ToString());
-			Console.WriteLine(result.GetType());
-			
+						
 		}
 		catch(NpgsqlException e)
 		{
