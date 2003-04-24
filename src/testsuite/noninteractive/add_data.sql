5b1de1673107f7897445fdaab6585038dd894702
fxjr <>
2003-04-24 01:37:26 +0000

    Added data for new types support

--- src/testsuite/noninteractive/add_data.sql
+++ src/testsuite/noninteractive/add_data.sql
@@ -13,3 +13,9 @@ insert into tableA(field_text) values ('Random text');
 insert into tableA(field_int4) values (4);
 insert into tableA(field_int8) values (8);
 insert into tableA(field_bool) values (true);
+
+insert into tableB(field_int2) values (2);
+insert into tableB(field_timestamp) values ('2002-02-02 09:00:23.345');
+insert into tableB(field_numeric) values (4.23);
+insert into tableB(field_numeric) values (-4.3);
+
