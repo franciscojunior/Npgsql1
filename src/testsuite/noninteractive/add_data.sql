706b9e659db9a669e438e16871e2bdfa7203c882
fxjr <>
2003-05-09 00:44:58 +0000

    Added data for tests with string with quotes.

--- src/testsuite/noninteractive/add_data.sql
+++ src/testsuite/noninteractive/add_data.sql
@@ -13,6 +13,7 @@ insert into tableA(field_text) values ('Random text');
 insert into tableA(field_int4) values (4);
 insert into tableA(field_int8) values (8);
 insert into tableA(field_bool) values (true);
+insert into tableA(field_text) values ('Text with \' single quote');
 
 insert into tableB(field_int2) values (2);
 insert into tableB(field_timestamp) values ('2002-02-02 09:00:23.345');
