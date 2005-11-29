74ef11ffa516f6afd45f81cdb4f36233297b52a8
fxjr <>
2005-11-29 14:07:29 +0000

    2005-11-29  Francisco Figueiredo Jr.  <fxjrlists@yahoo.com.br>
    	* NpgsqlParameter.cs,
    	NpgsqlCommandBuilder.cs: [gborg 1392] Fixed case sensitivity problems when using NpgsqlCommandBuilder. Now, all field and table names are properly quoted. Thanks Gridnine Systems (www.gridnine.com) for patch. [#1000447]

--- src/Npgsql/NpgsqlParameter.cs
+++ src/Npgsql/NpgsqlParameter.cs
@@ -120,7 +120,21 @@ namespace Npgsql
 
             }
         }
-
+		
+		/// <summary>
+		/// Internal constructor to handle parameter creation from CommandBuilder passing a NpgsqlNativeTypeInfo directly.
+		/// </summary>
+		internal NpgsqlParameter(String parameterName, object value, NpgsqlNativeTypeInfo type_info)
+		{
+			resman = new System.Resources.ResourceManager(this.GetType());
+			NpgsqlEventLog.LogMethodEnter(LogLevel.Debug, CLASSNAME, CLASSNAME, parameterName, value, type_info);
+			
+			this.ParameterName = parameterName;
+			this.value = (value == null) ? DBNull.Value : value;
+			
+			this.type_info = (type_info == null) ? NpgsqlTypesHelper.GetNativeTypeInfo(typeof(String)) : type_info;
+		}
+		
         /// <summary>
         /// Initializes a new instance of the <see cref="Npgsql.NpgsqlParameter">NpgsqlParameter</see>
         /// class with the parameter name and the data type.
